add8a453c4ca9a581207a24d2ca084caed64eaf8
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-21 12:14:40 +0800

    Pseudocode

--- DECRYPTOR.py
+++ DECRYPTOR.py
@@ -1,1 +1,20 @@
-'''TRIAL'''
+'''PSEUDOCODE
+START
+HEADING/PROMPT
+GET user's encrypted TEXT
+FOR char IN encypted TEXT
+    IF char in encypted TEXT = "*"
+       REPLACE with "a"
+    ELIF char in encypted TEXT = "&"
+       REPLACE with "e"
+    ELIF char in encypted TEXT = "#"
+       REPLACE with "i"
+    ELIF char in encypted TEXT = "+"
+       REPLACE with "o"
+    ELIF char in encypted TEXT = "!"
+       REPLACE with "u"
+    ELSE 
+    DISPLAY Encrypted TEXT
+
+PRINT Decrpted TEXT    
+'''
