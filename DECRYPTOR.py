1066422c74e064effa741a794c9af9c5d1f83f7b
unknown <alexapbabiera@iskolarngbayan.pup.edu.ph>
2023-04-21 18:04:21 +0800

    Looping and replacing

--- DECRYPTOR.py
+++ DECRYPTOR.py
@@ -27,3 +27,27 @@ print(Fore.YELLOW + "=" * 100)
 message = input(
     Fore.CYAN + "Enter an encrypted message \n(ex. th& q!#ck br+wn f+x ) : ")
 changed_char = ""
+
+# loop the message to check each character
+for i in range(len(message)):
+    # replace characters
+    # replace * with a
+    if message[i] == "*":
+        # set a control :
+        changed_char += "a"
+    # replace & with e
+    elif message[i] == "&":
+        changed_char += "e"
+    # replace # with i
+    elif message[i] == "#":
+        changed_char += "i"
+    # replace + with 0
+    elif message[i] == "+":
+        # set a control
+        changed_char += "o"
+    # replace ! with u
+    elif message[i] == "!":
+        # set a control
+        changed_char += "u"
+    else:
+        changed_char += message[i]
